033a37ed94c777923b8b4418073f4aa7c38edd67
Daniel Cacheta <danielcacheta@yahoo.com.br>
2019-03-09 18:56:29 -0300

    Hello World com pug

--- app.js
+++ app.js
@@ -13,11 +13,14 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
   // res.send('Hello from my library app');
   // res.sendFile(__dirname + '/views/index.html');
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index'); // rederizar uma view que chame 'index'
 });
 
 app.listen(port, () => {
