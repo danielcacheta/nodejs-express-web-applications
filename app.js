6d2cee721c6ce92bcc15bd5a8daa72b0a0747874
Daniel Cacheta <danielcacheta@yahoo.com.br>
2019-03-09 17:47:16 -0300

    Serve arquivos estáticos direto da node_modules quando não estão na pasta public

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var app = express();
 
 app.use(morgan('tiny'));
 
-app.use(express.static(path.join(__dirname, '/public/')))
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
     // res.send('Hello from my library app');
