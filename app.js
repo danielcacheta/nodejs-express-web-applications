ffb4172991dd54189f88048e9ff9b8cb71001704
Daniel Cacheta <danielcacheta@yahoo.com.br>
2019-03-09 18:20:35 -0300

    Corrige manualmente erros apontados pelo ESLint

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 
@@ -14,12 +14,13 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
-    // res.send('Hello from my library app');
-    // res.sendFile(__dirname + '/views/index.html');
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+  // res.send('Hello from my library app');
+  // res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
 
 app.listen(3000, () => {
-    console.log(`Listening on port ${chalk.green("3000")}`)
-    debug(`Message from DEBUG: Listening on port ${chalk.green("3000")}`)
-})
+  // eslint-disable-next-line no-console
+  console.log(`Listening on port ${chalk.green('3000')}`);
+  debug(`Message from DEBUG: Listening on port ${chalk.green('3000')}`);
+});
