931c3a431ebb3e96bbc30b346d4e113849f2573e
Daniel Cacheta <danielcacheta@yahoo.com.br>
2019-03-17 16:46:01 -0300

    Inclui view para books

--- app.js
+++ app.js
@@ -18,9 +18,45 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'Title 1',
+    genre: 'Genre 1',
+    author: 'Author 1',
+    read: false
+  },
+  {
+    title: 'Title 2',
+    genre: 'Genre 1',
+    author: 'Author 2',
+    read: false
+  },
+  {
+    title: 'Title 3',
+    genre: 'Genre 2',
+    author: 'Author 3',
+    read: false
+  },
+  {
+    title: 'Title 4',
+    genre: 'Genre 3',
+    author: 'Author 1',
+    read: false
+  },
+];
+
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('hello books');
+    res.render('books',
+      {
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }
+        ],
+        title: 'Library',
+        books
+      }
+    );
   });
 
 bookRouter.route('/single')
@@ -33,8 +69,10 @@ app.get('/', (req, res) => {
   res.render(
     'index',
     {
-      nav: [{ link: '/books', title: 'Books' },
-      { link: '/authors', title: 'Authors' }],
+      nav: [
+        { link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }
+      ],
       title: 'Library'
     }
   ); // rederizar uma view que chame 'index'
